816816de082bde91d7192d19012ab629aac24009
Hyeok_Oh <oh4851@gmail.com>
2016-01-11 17:15:46 +0900

    modify scales source code, add merge sorting

--- backtracking/3_scales/scale.c
+++ backtracking/3_scales/scale.c
@@ -7,7 +7,56 @@ int getBinaryPosition(int value, int position) {
     for (i=0; i<position; i++) {
         value = value >> 1;
     }
-    return value & 0x00000001;}
+    return value & 0x00000001;
+}
+
+void mergeSort(int *data, int n) {
+    int i, j, mid, now;
+    int temp[30];
+    for (i=0; i<n; i++) {
+        temp[i] = data[i];
+    }
+    mid = n/2;
+    // left(insertion)
+    for (i=1; i<mid; i++) {
+        now = temp[i];
+        j = i-1;
+        while ((j>=0) && (temp[j] < now)) {
+            temp[j+1] = temp[j];
+            j--;
+        }
+        temp[j+1] = now;
+    }
+    // right(insertion)
+    for (i=mid+1; i<n; i++) {
+        now = temp[i];
+        j = i-1;
+        while ((j>=mid) && (temp[j] < now)) {
+            temp[j+1] = temp[j];
+            j--;
+        }
+        temp[j+1] = now;
+    }
+    // merge
+    i = 0;
+    j = mid;
+    now = 0;
+
+    while (i < mid && j < n) {
+        if (temp[i] >= temp[j]) *(data+now++) = *(temp+i++);
+        else data[now++] = temp[j++];
+    }
+    if (i < mid) {
+        while (i < mid) {
+            data[now++] = temp[i++];
+        }
+    }
+    else if (j < n) {
+        while (j < n) {
+            data[now++] = temp[j++];
+        }
+    }
+}
 
 int main(void) {
     int i, j, n, sum, ans, pow, cnt;  // 1 <= n <= 30
@@ -33,6 +82,9 @@ int main(void) {
 
     // get half of sum
     ans = sum / 2;
+
+    // merge sort
+    mergeSort(weights, n);
 
     // get power of weights length
     pow = 1;
